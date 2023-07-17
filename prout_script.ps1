5535b14007e8fe933ee0dd7bd19df4b95d3264bd
RylexOff <60745761+RylexOff@users.noreply.github.com>
2023-07-17 15:56:16 +0200

    Update prout_script.ps1

--- prout_script.ps1
+++ prout_script.ps1
@@ -1,13 +1,20 @@
+#Increase the system volume by simulating pressing a volume up key
 $volume = New-Object -ComObject "WScript.Shell"
 $volumeLevel = 100
 for ($i = 0; $i -le $volumeLevel; $i++) {
     $volume.SendKeys([char]175)
 }
+
+#Set the download path in a variable
 $downloadPath = "$env:USERPROFILE\Downloads" 
-(New-Object System.Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/937329842859421706/1130437612985909298/Bruit_de_pet_qui_resonne.mp4', $downloadPath + '\Bruit_de_pet_qui_resonne.mp4')
+#Download the video of shrek who farts
+(New-Object System.Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/937329842859421706/1130437612985909298/Bruit_de_pet_qui_resonne.mp4', $downloadPath + '\shrek_Farts.mp4')
+#Get the path of the last downloaded file
 $videoPath = $(Get-ChildItem -Path $env:USERPROFILE\Downloads | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1).FullName
+#Open default media player
 $shell = New-Object -ComObject Shell.Application
 $shell.ShellExecute($videoPath)
+#clear terminal and quit
 clear
 exit
 
