40d75352f8792114b88d065db4a6107966f926f2
RylexOff <60745761+RylexOff@users.noreply.github.com>
2023-07-17 14:42:08 +0200

    Create prout_script.ps1

---
+++ prout_script.ps1
@@ -0,0 +1,7 @@
+$downloadPath = "$env:USERPROFILE\Downloads" 
+(New-Object System.Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/937329842859421706/1130437612985909298/Bruit_de_pet_qui_resonne.mp4', $downloadPath + '\Bruit_de_pet_qui_resonne.mp4')
+$videoPath = $(Get-ChildItem -Path $env:USERPROFILE\Downloads | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1).FullName
+$shell = New-Object -ComObject Shell.Application
+$shell.ShellExecute($videoPath)
+clear
+exit
