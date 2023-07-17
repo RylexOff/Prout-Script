877b75a1884a2d7861288e630d092cb1d3da2866
RylexOff <60745761+RylexOff@users.noreply.github.com>
2023-07-17 15:18:25 +0200

    Update prout_script.ps1

--- prout_script.ps1
+++ prout_script.ps1
@@ -1,3 +1,8 @@
+$volume = New-Object -ComObject "WScript.Shell"
+$volumeLevel = 100
+for ($i = 0; $i -le $volumeLevel; $i++) {
+    $volume.SendKeys([char]175)
+}
 $downloadPath = "$env:USERPROFILE\Downloads" 
 (New-Object System.Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/937329842859421706/1130437612985909298/Bruit_de_pet_qui_resonne.mp4', $downloadPath + '\Bruit_de_pet_qui_resonne.mp4')
 $videoPath = $(Get-ChildItem -Path $env:USERPROFILE\Downloads | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1).FullName
